4460fc02c25f131d0ede75f517700b4b1795ca1b
295872404 <295872404@qq.com>
2026-01-14 04:41:14 +0800

    BUG修复

--- common/defines/faction_defines.lua
+++ common/defines/faction_defines.lua
@@ -1,3 +1,3 @@
-NDefines.NFactions.MAX_NUM_SHORT_TERM_GOALS = 4						-- Maximum number of short term goals a faction can have at any one time	
-NDefines.NFactions.MAX_NUM_MEDIUM_TERM_GOALS = 2						-- Maximum number of medium term goals a faction can have at any one time
+NDefines.NFactions.MAX_NUM_SHORT_TERM_GOALS = 1						-- Maximum number of short term goals a faction can have at any one time	
+NDefines.NFactions.MAX_NUM_MEDIUM_TERM_GOALS = 1						-- Maximum number of medium term goals a faction can have at any one time
 NDefines.NFactions.MAX_NUM_LONG_TERM_GOALS = 1                       -- Maximum number of long term goals a faction can have at any one time
